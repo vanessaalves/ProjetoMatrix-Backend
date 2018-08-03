272aa5fd2f8a80c61250a42c60e46004d2944be1
vanessaalves <vns.alvess@gmail.com>
2018-08-03 13:11:17 -0300

    Melhorias no código

--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/AvaliadorAluno.java
@@ -1,25 +1,45 @@
 package br.com.cariocadev.ProjetoMatrix;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class AvaliadorAluno {
-
+	public void verificarNota(BigDecimal... notas) {
+		List<BigDecimal> NotasValidas = Stream.of(notas)
+				.filter(n -> n != null && n.compareTo(BigDecimal.ZERO) >= 0 && n.compareTo(BigDecimal.TEN) <= 0)
+				.collect(Collectors.toList());
+		if (NotasValidas.size() != 3)
+			throw new IllegalArgumentException();
+}
+	
 	public BigDecimal getMaiorNota(BigDecimal[] notas) {
-		// Implementação o código aqui
+		if (notas == null || notas.length == 0)
+			throw new IllegalArgumentException();
 		
-		return null;
+		Collections.sort(Arrays.asList(notas), Collections.reverseOrder());
+		return notas[0];
 	}
-
+	
 	public BigDecimal getMedia(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
+		verificarNota(nota1, nota2, nota3);
+		
+		return nota1.add(nota2.add(nota3)).divide(BigDecimal.valueOf(3), 2, RoundingMode.HALF_UP);
 	}
-
+	
 	public String getStatus(BigDecimal nota1, BigDecimal nota2, BigDecimal nota3) {
-		// Implementação o código aqui
-		return null;
-	}
-	
-	
-
+		verificarNota(nota1, nota2, nota3);
+		BigDecimal media = getMedia(nota1, nota2, nota3);
+		
+		if (media.compareTo(BigDecimal.valueOf(6)) >= 0)
+			return "APROVADO";
+		else if (media.compareTo(BigDecimal.valueOf(4)) < 0)
+			return "REPROVADO";
+		
+		return "PROVA_FINAL";
+	}	
 }
--- ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
+++ ProjetoMatrix/src/main/java/br/com/cariocadev/ProjetoMatrix/CalculadoraSalario.java
@@ -1,22 +1,60 @@
 package br.com.cariocadev.ProjetoMatrix;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CalculadoraSalario {
-
+	
+	public void verificarValor(BigDecimal salarioBruto) {
+		if (salarioBruto == null || salarioBruto.compareTo(BigDecimal.ZERO) == 0)
+			throw new IllegalArgumentException();
+	}
+	
+	public void verificarIdade(Integer idade) {
+		if (idade == null || idade < 0)
+			throw new IllegalArgumentException();
+	}
+	
+	public BigDecimal calculaDesconto(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
+		return salarioBruto.multiply(percentualImpostoINSS.divide(BigDecimal.valueOf(100))).setScale(2,
+				RoundingMode.HALF_UP);
+	}
+	
 	public BigDecimal getSalarioLiquido(BigDecimal salarioBruto, BigDecimal percentualImpostoINSS) {
-		// Implementar o código aqui
-		return null;
+		verificarValor(salarioBruto);
+		
+		return salarioBruto.subtract(calculaDesconto(salarioBruto, percentualImpostoINSS)).setScale(2,
+				RoundingMode.HALF_UP);
 	}
-
+	
 	public BigDecimal getValorINSS(BigDecimal salarioBruto) {
-		// Implementar o código aqui
-		return null;
+		verificarValor(salarioBruto);
+		
+		if (salarioBruto.compareTo(new BigDecimal(1693.72)) < 0)
+			return calculaDesconto(salarioBruto, BigDecimal.valueOf(8));
+		else if (salarioBruto.compareTo(new BigDecimal(2822.90)) < 0)
+			return calculaDesconto(salarioBruto, BigDecimal.valueOf(9));
+		
+		return calculaDesconto(salarioBruto, BigDecimal.valueOf(11));
 	}
-
+	
+	public Map<Integer, BigDecimal> IdadePrecoPlanoSaude = new HashMap<Integer, BigDecimal>();
+	
+	public void prepararMap() {
+		
+		double valor = 75.00;
+		for (int i = 0; i <= 60; i++) {
+			valor = i == 0 || i % 10 != 0 ? valor : (valor + (valor * 0.5));
+			IdadePrecoPlanoSaude.put(Integer.valueOf(i), BigDecimal.valueOf(valor).setScale(2, BigDecimal.ROUND_UP));
+		}
+	}
+	
 	public BigDecimal getValorPlanoDeSaude(Integer idade) {
-		// TODO Auto-generated method stub
-		return null;
+		verificarIdade(idade);
+		if (IdadePrecoPlanoSaude.isEmpty())
+			prepararMap();
+		return idade > 60 ? IdadePrecoPlanoSaude.get(60) : IdadePrecoPlanoSaude.get(idade);
 	}
-
 }
